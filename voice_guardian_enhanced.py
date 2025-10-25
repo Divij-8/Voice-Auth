70212055312bcc5ca9b3647ca2d38f6c5b4d2d7f
Divij Mazumdar <divij.mazumdar@gmail.com>
2025-10-25 18:37:02 +0530

    add

---
+++ voice_guardian_enhanced.py
@@ -0,0 +1,7 @@
+"""
+Voice Guardian Enhanced: Re-export from voice_guardian for FastAPI backend compatibility
+"""
+
+from voice_guardian import VoiceGuardianEnhanced, VoiceGuardian
+
+__all__ = ['VoiceGuardianEnhanced', 'VoiceGuardian']
